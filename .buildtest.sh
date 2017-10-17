c5e9b493402b006a084802d64cb272b7f09a81d4
Asim Jamshed <ajamshed@ndsl.kaist.edu>
2017-10-17 13:03:24 +0900

    - Updated travis-related script

--- .buildtest.sh
+++ .buildtest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-cd dpdk-16.11/
+cd dpdk-2.0.0/
 make install T=x86_64-native-linuxapp-gcc
 cd ..
 cd dpdk
 rm -rf *
-ln -s ../dpdk-16.11/x86_64-native-linuxapp-gcc/lib/ lib
-ln -s ../dpdk-16.11/x86_64-native-linuxapp-gcc/include include
+ln -s ../dpdk-2.0.0/x86_64-native-linuxapp-gcc/lib/ lib
+ln -s ../dpdk-2.0.0/x86_64-native-linuxapp-gcc/include include
 cd ..
 ./configure --with-dpdk-lib=`echo $PWD`/dpdk
 make
